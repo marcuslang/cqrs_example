e86365cab77beacbdf6478b14ddec2d495149af2
marcuslang <marcuslang@zoho.com>
2017-01-04 18:25:32 +0100

    init

---
+++ src/main/java/org/rapidpm/cqrs/example/HotelBookingUI.java
@@ -0,0 +1,20 @@
+package org.rapidpm.cqrs.example;
+
+
+import org.rapidpm.cqrs.example.model.Hotel;
+
+import java.util.Date;
+import java.util.List;
+
+public class HotelBookingUI {
+
+  public List<Hotel> getHotels() {
+    return null;
+  }
+
+  public void doBooking(String hotelId, Date from, Date to) {
+
+  }
+
+
+}
---
+++ src/main/java/org/rapidpm/cqrs/example/service/QueryService.java
@@ -0,0 +1,4 @@
+package org.rapidpm.cqrs.example.service;
+
+public interface QueryService {
+}
